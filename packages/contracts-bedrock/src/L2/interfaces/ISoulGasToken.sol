2d7a7c6ccd78de64d9ca3d6935e320c7da03835c
qzhodl <freedostudio@gmail.com>
2024-11-28 09:58:49 +0000

    semgrep rules

--- packages/contracts-bedrock/src/L2/interfaces/ISoulGasToken.sol
+++ packages/contracts-bedrock/src/L2/interfaces/ISoulGasToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 /// @title ISoulGasToken
 /// @notice The interface for the SoulGasToken.
 interface ISoulGasToken {
-    function initialize(string memory name_, string memory symbol_, address owner_) external;
+    function initialize(string memory _name, string memory _symbol, address _owner) external;
 
     function __constructor__() external;
 }

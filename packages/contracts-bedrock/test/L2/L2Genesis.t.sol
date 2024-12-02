0895321f6c5d9e73b3c6ec934df366ca36d6af71
qiang <freedostudio@gmail.com>
2024-12-02 18:35:56 +0800

    fix comment

--- packages/contracts-bedrock/test/L2/L2Genesis.t.sol
+++ packages/contracts-bedrock/test/L2/L2Genesis.t.sol
@@ -174,11 +174,15 @@ contract L2GenesisTest is Test {
         genesis.writeGenesisAllocs(_path);
 
         uint256 expected = 0;
-        expected += Predeploys.PREDEPLOY_COUNT; // predeploy proxies
-        expected += 18; // predeploy implementations (excl. legacy erc20-style eth and legacy message sender)
+        // predeploy proxies; WETH and GovernanceToken are not behind proxies
+        expected += Predeploys.PREDEPLOY_COUNT - 2;
+        // predeploy implementations (excl. legacy erc20-style eth and legacy message sender)
+        // setPredeployImplementations function in L2Genesis.s.sol sets 20 predeploy implementations (useInterop ==
+        // false)
+        expected += 20;
         expected += 256; // precompiles
+        // setPreinstalls function in SetPreinstalls.s.sol sets 15 preinstalls
         expected += 15; // preinstalls
-        expected += 1; // 4788 deployer account
         // 16 prefunded dev accounts are excluded
         assertEq(expected, getJSONKeyCount(_path), "key count check");
 

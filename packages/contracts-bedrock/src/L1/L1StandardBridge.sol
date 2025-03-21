5694137c514c2d8904021417c7d457b971b3db28
blockchaindevsh <shore.cloud@gmail.com>
2025-03-21 10:56:37 +0800

    update L1StandardBridge.sol

--- packages/contracts-bedrock/src/L1/L1StandardBridge.sol
+++ packages/contracts-bedrock/src/L1/L1StandardBridge.sol
@@ -6,6 +6,7 @@ import { StandardBridge } from "src/universal/StandardBridge.sol";
 
 // Libraries
 import { Predeploys } from "src/libraries/Predeploys.sol";
+import { Storage } from "src/libraries/Storage.sol";
 
 // Interfaces
 import { ISemver } from "interfaces/universal/ISemver.sol";
@@ -82,13 +83,14 @@ contract L1StandardBridge is StandardBridge, ISemver {
     /// @notice Address of the SuperchainConfig contract.
     ISuperchainConfig public superchainConfig;
 
-    // /// @custom:legacy
-    // /// @custom:spacer systemConfig
-    // /// @notice Spacer taking up the legacy `systemConfig` slot.
-    // address private spacer_51_0_20;
+    /// @notice Storage slot that the systemConfig address is stored at.
+    /// TODO_QKC: add upgrade logic with OPCM's latest upgrade mechanism
+    bytes32 public constant SYSTEM_CONFIG_SLOT = bytes32(uint256(keccak256("opstack.systemconfig")) - 1);
 
-    /// @notice Address of the SystemConfig contract.
-    ISystemConfig public systemConfig;
+    /// @custom:legacy
+    /// @custom:spacer systemConfig
+    /// @notice Spacer taking up the legacy `systemConfig` slot.
+    address private spacer_51_0_20;
 
     /// @notice Constructs the L1StandardBridge contract.
     constructor() StandardBridge() {
@@ -107,7 +109,7 @@ contract L1StandardBridge is StandardBridge, ISemver {
         initializer
     {
         superchainConfig = _superchainConfig;
-        systemConfig = _systemConfig;
+        Storage.setAddress(SYSTEM_CONFIG_SLOT, address(_systemConfig));
         __StandardBridge_init({
             _messenger: _messenger,
             _otherBridge: StandardBridge(payable(Predeploys.L2_STANDARD_BRIDGE))
@@ -126,7 +128,7 @@ contract L1StandardBridge is StandardBridge, ISemver {
 
     /// @inheritdoc StandardBridge
     function gasPayingToken() internal view override returns (address addr_, uint8 decimals_) {
-        (addr_, decimals_) = systemConfig.gasPayingToken();
+        (addr_, decimals_) = ISystemConfig(Storage.getAddress(SYSTEM_CONFIG_SLOT)).gasPayingToken();
     }
 
     /// @custom:legacy

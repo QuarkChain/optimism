2ccdc527ebfae05a1ccf17f99cb0c9ae2d5b9b93
blockchaindevsh <shore.cloud@gmail.com>
2025-04-16 20:46:12 +0800

    fix ISoulGasToken.__constructor__

--- packages/contracts-bedrock/interfaces/L2/ISoulGasToken.sol
+++ packages/contracts-bedrock/interfaces/L2/ISoulGasToken.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 interface ISoulGasToken {
     function initialize(string memory _name, string memory _symbol, address _owner) external;
 
-    function __constructor__() external;
+    function __constructor__(bool _isBackedByNative) external;
 
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);

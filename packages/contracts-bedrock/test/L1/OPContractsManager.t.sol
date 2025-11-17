13bc5a7e9e33e74d43cbdfcf4b56420700e83ac1
syntrust <lundeng@quarkchain.org>
2025-11-17 19:45:18 +0800

    fix not set

--- packages/contracts-bedrock/test/L1/OPContractsManager.t.sol
+++ packages/contracts-bedrock/test/L1/OPContractsManager.t.sol
@@ -1635,6 +1635,7 @@ contract OPContractsManager_Deploy_Test is DeployOPChain_TestBase {
         doi.set(doi.disputeSplitDepth.selector, disputeSplitDepth);
         doi.set(doi.disputeClockExtension.selector, disputeClockExtension);
         doi.set(doi.disputeMaxClockDuration.selector, disputeMaxClockDuration);
+        doi.set(doi.batchInbox.selector, opcm.chainIdToBatchInboxAddress(l2ChainId));
     }
 
     // This helper function is used to convert the input struct type defined in DeployOPChain.s.sol
